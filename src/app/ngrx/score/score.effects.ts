0e93a44586dda4fca60149703742bf32fb011158
rsignes <romain.signes@gmail.com>
2021-11-02 08:07:40 +0100

    first effect

--- src/app/ngrx/score/score.effects.ts
+++ src/app/ngrx/score/score.effects.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { ActionTypes } from './score.actions';
+import { map } from 'rxjs/operators';
+import { ActionTypes, edit } from './score.actions';
 
 @Injectable()
-export class MovieEffects {
-  // loadMovies$ = createEffect(() =>
-  //   this.actions$.pipe(ofType(ActionTypes.Edit))
-  // );
+export class ScoreEffects {
+  editScore$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ActionTypes.Edit),
+      // 📤 Les tâches asynchrones peuvent être exécutées ici
+      map(({ score }) => {
+        return edit({ score });
+      })
+    )
+  );
 
   constructor(private actions$: Actions) {}
 }
